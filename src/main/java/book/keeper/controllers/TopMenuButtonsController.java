f4b0987aefd665113c2292612005f99687e30da6
AJuszczakiewicz <al.jusz@gmail.com>
2019-06-07 03:54:39 +0200

    Add mock forms

--- src/main/java/book/keeper/controllers/TopMenuButtonsController.java
+++ src/main/java/book/keeper/controllers/TopMenuButtonsController.java
@@ -2,27 +2,50 @@ package book.keeper.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
 
 public class TopMenuButtonsController {
 
+    public static final String FXML_LIBRARY_PATH = "/fxml/Library.fxml";
+    public static final String FXML_BOOKS_LIST_PATH = "/fxml/BooksList.fxml";
+    public static final String FXML_STATISTICS_PATH = "/fxml/Statistics.fxml";
+    public static final String FXML_ADD_BOOK_PATH = "/fxml/AddBook.fxml";
+
     private MainController mainController;
+
+    @FXML
+    private ToggleGroup menu;
 
     public void setMainController(MainController mainController) {
         this.mainController = mainController;
     }
 
     @FXML
-    public void openLibrary(ActionEvent actionEvent) {
-        System.out.println("Open Library");
+    public void openLibrary() {
+
+        mainController.setCenter(FXML_LIBRARY_PATH);
     }
 
     @FXML
-    public void openBookList(ActionEvent actionEvent) {
-        System.out.println("Open Book List");
+    public void openBookList() {
+
+        mainController.setCenter(FXML_BOOKS_LIST_PATH);
     }
 
     @FXML
-    public void openStatistics(ActionEvent actionEvent) {
-        System.out.println("Open Statistics");
+    public void openStatistics() {
+
+        mainController.setCenter(FXML_STATISTICS_PATH);
+    }
+
+    @FXML
+    public void addBook() {
+        if (menu.getSelectedToggle() != null) {
+            menu.getSelectedToggle().setSelected(false);
+        }
+
+        mainController.setCenter(FXML_ADD_BOOK_PATH);
+
     }
 }
