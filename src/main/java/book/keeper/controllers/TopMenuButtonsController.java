987fc9a501a1c48560b18449f8e9e31e03e3f93f
AJuszczakiewicz <ajuszczakiewicz@protonmail.com>
2019-06-15 10:54:20 +0200

    Cleanup

--- src/main/java/book/keeper/controllers/TopMenuButtonsController.java
+++ src/main/java/book/keeper/controllers/TopMenuButtonsController.java
@@ -5,9 +5,7 @@ import javafx.scene.control.ToggleGroup;
 
 public class TopMenuButtonsController {
 
-    public static final String FXML_LIBRARY_PATH = "/fxml/Library.fxml";
     public static final String FXML_BOOKS_LIST_PATH = "/fxml/BooksList.fxml";
-    public static final String FXML_STATISTICS_PATH = "/fxml/Statistics.fxml";
     public static final String FXML_ADD_BOOK_PATH = "/fxml/AddBook.fxml";
     public static final String FXML_ADD_CATEGORY_PATH = "/fxml/AddCategory.fxml";
     private static final String ADD_AUTHOR_FXML = "/fxml/AddAuthor.fxml";
@@ -21,20 +19,12 @@ public class TopMenuButtonsController {
         this.mainController = mainController;
     }
 
-    @FXML
-    public void openLibrary() {
-        mainController.setCenter(FXML_LIBRARY_PATH);
-    }
 
     @FXML
     public void openBookList() {
         mainController.setCenter(FXML_BOOKS_LIST_PATH);
     }
 
-    @FXML
-    public void openStatistics() {
-        mainController.setCenter(FXML_STATISTICS_PATH);
-    }
 
     @FXML
     public void addBook() {
